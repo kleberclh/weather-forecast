14bfe5dc0be0c7cda27e1a21d1100c8b7fc0c79c
Kleber Coelho <klebercoelhogt@gmail.com>
2024-11-17 21:26:52 -0300

    #

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // URL base da API OpenWeatherMap para consultar o tempo
 const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-const KEY = "5b42af90f7416dbbd5e04525c617fec7"; // Sua chave de API
+const KEY = "5b42af90f7416dbbd5e04525c617fec7"; 
 
 const api = axios.create({
   baseURL: API_URL,
@@ -14,8 +14,8 @@ export async function getWeather(city) {
       params: {
         q: city,
         appid: KEY,
-        units: "metric", // Temperatura em Celsius
-        lang: "pt", // Idioma
+        units: "metric", 
+        lang: "pt", 
       },
     });
     return response.data;
